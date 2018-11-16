c609f74626fe7b6979a6d7cca41a2060c709b168
Tracie Masek <traciemasek@gmail.com>
2018-11-16 23:58:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,7 +18,11 @@ function whileLoop(number) {
 }
 
 function doWhileLoop(array) {
-  
+  var i = 0;
+ 
+  function incrementVariable() {
+  i = i + 1;
+}
   do {
     array.pop;
   } while ([array.length > 0 && incrementVariable()])
